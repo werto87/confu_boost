25b74be234a6789bcf6fe8f5ae33a873e5d067bc
werto87 <werto87>
2021-03-03 20:35:52 +0000

    test case text to struct

--- test/test.cxx
+++ test/test.cxx
@@ -6,17 +6,28 @@
 #include <string>
 
 BOOST_FUSION_DEFINE_STRUCT ((database), Character, (std::string, id) (std::string, positionId) (std::string, accountId))
-
 BOOST_SERIALIZATION_BOILER_PLATE (database::Character)
-
 namespace test
 {
 
-TEST_CASE ("type name with out namespace")
+TEST_CASE ("struct to text")
 {
   auto characterStringStream = std::stringstream{};
   boost::archive::text_oarchive characterArchive{ characterStringStream };
   characterArchive << database::Character{ .id = "id", .positionId = "positionId", .accountId = "accountId" };
   REQUIRE (characterStringStream.str () == "22 serialization::archive 18 0 0 2 id 10 positionId 9 accountId");
 }
+
+TEST_CASE ("text to struct")
+{
+  auto characterStringStream = std::stringstream{};
+  characterStringStream << "22 serialization::archive 18 0 0 2 id 10 positionId 9 accountId";
+  boost::archive::text_iarchive ia (characterStringStream);
+  auto character = database::Character{};
+  ia >> character;
+  REQUIRE (character.id == "id");
+  REQUIRE (character.positionId == "positionId");
+  REQUIRE (character.accountId == "accountId");
+}
+
 }
